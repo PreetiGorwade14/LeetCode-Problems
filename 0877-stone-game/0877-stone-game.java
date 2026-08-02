0596c4281801c5571575f6557b9a5bba61caec29
PreetiGorwade14 <preetigorwade14@gmail.com>
2026-08-02 11:48:56 +0530

    Time: 8 ms (18.78%), Space: 46.7 MB (16.42%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int dp[][] = new int[piles.length][piles.length];
+
+        return solve(piles, 0, piles.length-1, dp) > 0;
+    }
+
+    public int solve(int[] piles, int i, int j, int dp[][]){
+        if( i== j){
+            return piles[i];
+        }
+
+        if(dp[i][j]!=0){
+            return dp[i][j];
+        }
+
+        int left = piles[i] - solve(piles, i+1, j, dp);
+        int right = piles[j] - solve(piles, i, j-1, dp);
+
+        dp[i][j] = Math.max(left, right);
+
+        return dp[i][j];
+    }
+}
